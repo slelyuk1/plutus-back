eab7735ae68cbeae257aa155505cc6c72cb4e139
Oleksandr Leliuk <slelyuk1@gmail.com>
2020-10-16 23:12:41 +0300

    Non-important changes

--- src/main/java/com/plutus/system/repository/AccountRepository.java
+++ src/main/java/com/plutus/system/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package com.plutus.system.repository;
 
 import com.plutus.system.model.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
--- src/main/java/com/plutus/system/repository/ClientRepository.java
+++ src/main/java/com/plutus/system/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.plutus.system.repository;
 
 import com.plutus.system.model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
