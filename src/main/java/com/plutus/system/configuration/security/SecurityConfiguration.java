4fd358400bbc65681d352be1b0c27692b315926c
Oleksandr Leliuk <slelyuk1@gmail.com>
2021-03-02 20:45:13 +0200

    altered security configuration

--- src/main/java/com/plutus/system/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/plutus/system/configuration/security/SecurityConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 
@@ -51,10 +52,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // todo enable cors and csrf
         http
-                .csrf().csrfTokenRepository(csrfTokenRepositoryBean()).ignoringAntMatchers(TOKEN_ENDPOINT)
-                .and()
-                .cors().and()
+                .csrf().csrfTokenRepository(csrfTokenRepositoryBean()).ignoringAntMatchers(TOKEN_ENDPOINT).disable()
+                .cors().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
